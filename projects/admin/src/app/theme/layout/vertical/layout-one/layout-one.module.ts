15e3b6b0a05c5cd5ad9e1031a3827d739927ad13
Prabakaran <vscodeguru@gmail.com>
2021-04-05 15:11:15 +0530

    layout support component created

--- projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.module.ts
+++ projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.module.ts
@@ -7,12 +7,20 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { GuruCardModule } from '@guru/card';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule, MatSidenavModule, FlexLayoutModule, MatToolbarModule, GuruCardModule
   ],
-  declarations: [VerticalLayoutOneComponent],
+  declarations: [
+    VerticalLayoutOneComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
   exports: [VerticalLayoutOneComponent],
 })
 export class VerticalLayoutOneModule { }
