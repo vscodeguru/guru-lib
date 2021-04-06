019cb743c60b4791ed7d8335179ea7261bfc36b3
Prabakaran <vscodeguru@gmail.com>
2021-04-06 22:10:57 +0530

    dynamic menu almost done

--- projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.component.ts
+++ projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.component.ts
@@ -1,19 +1,43 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { GuruHeaderFooterPosition } from '@guru/card';
+import { LazyService } from '../../../service/LazyService';
 
 @Component({
   selector: 'vertical-layout-one',
   templateUrl: './layout-one.component.html',
-  styleUrls: ['./layout-one.component.scss'],
+  styleUrls: ['./layout-one.component.less'],
   encapsulation: ViewEncapsulation.None
 })
 export class VerticalLayoutOneComponent implements OnInit {
   headerPosition: GuruHeaderFooterPosition = 'below-static';
   footerPosition: GuruHeaderFooterPosition = 'below-static';
   showHeder = true;
+  mlTheme: ITheme = {
+    Sidebar: {
+      'layout-sider-background': { desc: 'Sidebar Backgroud', default: '#0f172a' },
+      'menu-item-color': { desc: 'Sidebar Backgroud', default: '#ec7070' },
 
-  constructor() { }
+      'menu-item-active-bg': { desc: 'Current Menu Backgroud', default: 'yellow' },
+      'menu-item-active-color': { desc: 'Current Menu Backgroud', default: 'black' },
+
+
+      'menu-item-highlight-bg': { desc: 'Hover Menu Backgroud', default: 'red' },
+      'menu-item-highlight-color': { desc: 'Hover Menu Color', default: 'blue' }
+    }
+  }
+  data: any = {};
+  loadedLess = false;
+  private get validSidebarKeys(): string[] {
+    return Object.keys(this.mlTheme?.Sidebar || {});
+  }
+  constructor(
+    private lazy: LazyService,
+    @Inject(DOCUMENT) private doc: any
+  ) { }
   ngOnInit(): void {
+    this.runLess();
   }
 
 
@@ -24,4 +48,52 @@ export class VerticalLayoutOneComponent implements OnInit {
   log(msg: string): void {
     console.log(msg);
   }
+  preview(): void {
+    this.runLess();
+  }
+  private async loadLess(): Promise<void> {
+    if (this.loadedLess) {
+      return Promise.resolve();
+    }
+    return this.lazy
+      .loadStyle('./assets/theme/menu.less', 'stylesheet/less')
+      .then(() => {
+        const lessConfigNode = this.doc.createElement('script');
+        lessConfigNode.innerHTML = `
+          window.less = {
+            async: true,
+            env: 'production',
+            javascriptEnabled: true
+          };
+        `;
+        this.doc.body.appendChild(lessConfigNode);
+      })
+      .then(() => this.lazy.loadScript('https://gw.alipayobjects.com/os/lib/less.js/3.8.1/less.min.js'))
+      .then(() => {
+        this.loadedLess = true;
+      });
+  }
+  private runLess(): void {
+    this.loadLess().then(() => {
+      (window as any).less.modifyVars(this.genVars()).then(() => {
+
+      });
+    });
+  }
+  private genVars(): any {
+    const vars: any = {};
+    this.validSidebarKeys.forEach(key => (vars[`@${key}`] = this.mlTheme.Sidebar[key].default));
+    return vars;
+  }
+}
+
+interface ITheme {
+  Sidebar: { [key: string]: IStyle };
+  header?: IStyle[];
+  footer?: IStyle[];
+}
+
+interface IStyle {
+  desc: string;
+  default: string;
 }
--- projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.module.ts
+++ projects/admin/src/app/theme/layout/vertical/layout-one/layout-one.module.ts
@@ -4,17 +4,22 @@ import { VerticalLayoutOneComponent } from './layout-one.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { GuruCardModule } from '@guru/card';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MatrixSidebarModule } from '../../components/sidebar/sidebar.module';
-
+import { FormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ColorPickerModule } from 'ngx-color-picker';
 @NgModule({
   imports: [
-    CommonModule, MatSidenavModule, FlexLayoutModule, MatToolbarModule, GuruCardModule,
-    MatrixSidebarModule
+    CommonModule,
+    FormsModule,
+    FlexLayoutModule,
+    GuruCardModule,
+    MatrixSidebarModule,
+    NzFormModule,
+    ColorPickerModule
   ],
   declarations: [
     VerticalLayoutOneComponent,
