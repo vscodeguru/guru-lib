a4743ce907aecd198b7feabe3a614546403996bf
Prabakaran <vscodeguru@gmail.com>
2021-04-09 19:31:12 +0530

    loading screen & path resolve issue fixed

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { LoadingService } from './core/service/loading.service';
+
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'admin';
+  constructor(
+    private _loading: LoadingService
+  ) { }
+
+  ngOnInit(): void { }
+  ngAfterViewInit(): void {
+    this._loading.hide();
+  }
 }
